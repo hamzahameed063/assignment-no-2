eeeb3c2315ba1b0d52bcaf628595915d9cde16d3
hamzahameed063 <75170988+hamzahameed063@users.noreply.github.com>
2020-12-28 22:20:51 +0500

    done

--- myJsCode.js
+++ myJsCode.js
@@ -3,9 +3,19 @@ $(function(){
 });
 
 function handleBtnClick(){
-  var input = $(".form-control").val();
-  console.log(input);
-  if(!input){
-    $(".form-control").addClass("error");
+  var einput = $(".email-input").val();
+  var pinput = $(".password-input").val();
+  if(!einput){
+    $(".email-input").addClass("error");
+    $(".email-error").addClass("error-text");
+    $(".email-error").html("**Filled is empty");
+    return false;
   }
+  if(!pinput){
+    $(".password-input").addClass("error");
+    $(".password-error").addClass("error-text");
+    $(".password-error").html("**Filled is empty");
+    return false;
+  }
+
 }
